7ed66015e8c24dd19444a5eb0a87f50ac9e928b2
thiswildidea <gaqo@outlook.com>
2020-07-31 14:36:24 +0800

    分离task模块

--- src/Plugins.ts
+++ src/Plugins.ts
@@ -2,7 +2,6 @@ import Boundary from './Boundary';
 import Layers from './interface/Layers';
 import MigrationMap from './MigrationMap';
 import { load } from './modules';
-import Task from './Task';
 import Trajectory from './Trajectory';
 import TrajectoryPlus from './TrajectoryPlus';
 export default {
@@ -10,7 +9,6 @@ export default {
   Layers,
   Boundary,
   TrajectoryPlus,
-  Task,
   MigrationMap,
   Trajectory
 };
--- src/interface/IQueryOptions.ts
+++
@@ -1,7 +0,0 @@
-
-export default interface IQueryOptions {
-    LayerID?: any;
-    queryDefinition?: string;
-    symbol?: any;
-    displayed ?: boolean;
-}
